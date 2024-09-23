a42b2b7bc2bf0320aae0e9b7c5e177a9a85e9cb6
Muhammad Nawzad <hama127n@gmail.com>
2024-09-23 08:25:51 +0300

    Updates profile page

--- src/apps/auth/models/user.cr
+++ src/apps/auth/models/user.cr
@@ -2,5 +2,9 @@ module Auth
   class User < MartenAuth::User
     field :first_name, :string, max_size: 128
     field :last_name, :string, max_size: 128
+
+    def full_name
+      "#{first_name} #{last_name}"
+    end
   end
 end
