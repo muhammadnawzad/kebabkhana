c23e4054952dfcf93b355ba113b60d7f4eb8fd37
Muhammad Nawzad <hama127n@gmail.com>
2024-09-24 15:45:07 +0300

    Updates routes

--- src/apps/kebab/routes.cr
+++ src/apps/kebab/routes.cr
@@ -1,5 +1,8 @@
 module Kebab
   ROUTES = Marten::Routing::Map.draw do
     path "/orders", ListOrdersHandler, name: "orders"
+    path "/create-batch", Batches::CreateHandler, name: "create_batch"
+    path "/list-batches", Batches::ListHandler, name: "list_batches"
+    path "/update-batch/<pk:int>", Batches::UpdateHandler, name: "update_batch"
   end
 end
