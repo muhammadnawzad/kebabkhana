cffc65338b71d424feb8cdd25254fd671d94c334
Muhammad Nawzad <hama127n@gmail.com>
2024-09-24 15:35:48 +0300

    Adds batch model

---
+++ src/apps/kebab/models/batch.cr
@@ -0,0 +1,28 @@
+module Kebab
+  class Batch < Marten::Model
+    field :id, :big_int, primary_key: true, auto: true
+    field :name, :string, max_size: 255, unique: true, index: true
+    field :status, :string, max_size: 255, default: "active", null: false, index: true
+    with_timestamp_fields
+
+    def active? : Bool 
+      status == "active"
+    end
+
+    def inactive? : Bool
+      status == "inactive"
+    end
+
+    def active : Kebab::Batch::QuerySet
+      Batch.filter(status: "active")
+    end
+
+    def inactive : Kebab::Batch::QuerySet
+      Batch.filter(status: "inactive")
+    end
+
+    def self.statuses : Array(String)
+      ["active", "inactive"]
+    end
+  end
+end
