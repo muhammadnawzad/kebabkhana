d6d66f8dc7f995046b376599a3495c7f90896123
Muhammad Nawzad <hama127n@gmail.com>
2024-09-30 11:49:03 +0300

    Adds pending payment warnings

--- src/apps/auth/handlers/profile_handler.cr
+++ src/apps/auth/handlers/profile_handler.cr
@@ -6,7 +6,13 @@ module Auth
 
     include RequireSignedInUser
     include NavItemActivateable
+
+    before_render :calculate_pending_payments
     
     template_name "auth/profile.html"
+
+    private def calculate_pending_payments
+      context[:pending_payments] = Order.filter(user_id: request.user!.id, status: "unpaid").sum(:total) || 0
+    end
   end
 end
--- src/apps/management/handlers/order_landing_handler.cr
+++ src/apps/management/handlers/order_landing_handler.cr
@@ -6,7 +6,11 @@ module Management
     include NavItemActivateable
 
     def get
-      render("home/index.html")
+      active_batch = Batch.active
+      has_already_ordered = Order.filter(user_id: request.user!.id, batch_id: active_batch.first.not_nil!.id).count > 0
+      pending_payments = Order.filter(user_id: request.user!.id, status: "unpaid").sum(:total) || 0
+
+      render("home/index.html", context: {has_already_ordered: has_already_ordered, pending_payments: pending_payments})
     end
   end
 end
