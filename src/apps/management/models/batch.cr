e07ec6ec94d521bc3c9575529e36fa0fea2b5c84
Muhammad Nawzad <hama127n@gmail.com>
2024-10-01 12:15:01 +0300

    Updates batches list to include items and prices

--- src/apps/management/models/batch.cr
+++ src/apps/management/models/batch.cr
@@ -4,6 +4,45 @@ class Batch < Marten::Model
   field :status, :string, max_size: 255, default: "active", null: false, index: true
   with_timestamp_fields
 
+  def order_count : Float64 | Int32
+    count = orders.count
+    # Ensure the count is cast to the correct type
+    if count.is_a?(Int64)
+      count.to_i # Convert Int64 to Int32
+    else
+      count
+    end
+  end
+
+  def total : Float64 | Int32
+    orders.sum(:total) || 0
+  end
+
+  def total_paid : Float64 | Int32
+    orders.filter(status: "paid").sum(:total) || 0
+  end
+
+  def items : Hash(String, Int32)
+    # Initialize the hash to store item name and its total quantity
+    items_count = {} of String => Int32
+
+    # Prefetch orders, order_items, and items
+    orders = Order.filter(batch_id: self.id).prefetch(:order_items__item)
+
+    # Loop through each order and order_item to accumulate item counts
+    orders.each do |order|
+      order.order_items.each do |order_item|
+        # Safe navigation to ensure item exists
+        if item_name = order_item.item!.name
+          items_count[item_name] ||= 0
+          items_count[item_name] += (order_item.quantity || 0)
+        end
+      end
+    end
+
+    items_count
+  end
+
   def active? : Bool
     status == "active"
   end
--- src/apps/management/models/order.cr
+++ src/apps/management/models/order.cr
@@ -3,16 +3,18 @@ class Order < Marten::Model
   field :total, :int, null: false
   field :status, :string, max_size: 255, null: false
   field :user, :many_to_one, to: Auth::User
-  field :batch, :many_to_one, to: Batch
+  field :batch, :many_to_one, to: Batch, related: :orders
   with_timestamp_fields
 
   # Associations
-  def items : Array(Item) | Nil
+  def items : Array(Item)
+    items_available = [] of Item
+    
     items_available = Item.all.raw("SELECT items.* FROM items JOIN order_items ON order_items.item_id = items.id WHERE order_items.order_id = ?", id).each do |item|
       item
     end
 
-    items_available || nil
+    items_available || [] of Item
   end
 
   # Status methods
