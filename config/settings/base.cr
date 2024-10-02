e4c74b26a6205ae84542f8bf4a1f80cdb09eaaff
Muhammad Nawzad <hama127n@gmail.com>
2024-10-03 00:01:19 +0300

    Updates environments

--- config/settings/base.cr
+++ config/settings/base.cr
@@ -2,7 +2,7 @@ Dotenv.load ".env" if File.exists?(".env")
 
 Marten.configure do |config|
   # IMPORTANT: please ensure that the secret key value is kept secret!
-  config.secret_key = ENV["SELF__SECRET_KEY_BASE"]
+  config.secret_key = "__insecure_d6c626abd975a5544f713ad0af727f77f75f4f7fcc02e2447f7a776d8b3bafc9__"
 
   # Installed applications
   # https://martenframework.com/docs/development/reference/settings#installed_apps
@@ -26,10 +26,10 @@ Marten.configure do |config|
   # https://martenframework.com/docs/development/reference/settings#database-settings
   config.database do |db| # ameba:disable Naming/BlockParameterName
     db.backend = :postgresql
-    db.host = ENV["DATABASE__HOST"]
-    db.name = ENV["DATABASE__NAME"]
-    db.user = ENV["DATABASE__USERNAME"]
-    db.password = ENV["DATABASE__PASSWORD"]
+    db.host = "localhost"
+    db.name = "kebabkhana_development"
+    db.user = "postgres"
+    db.password = "postgres"
     db.port = 5432
   end
 
--- config/settings/production.cr
+++ config/settings/production.cr
@@ -2,8 +2,8 @@ Marten.configure :production do |config|
   # Application Settings
   config.host = "0.0.0.0"
   config.port = 3000
-  config.allowed_hosts = ENV.fetch("SELF__ALLOWED_HOSTS", "").split(",")
-  config.debug = true
+  onfig.secret_key = ENV.fetch("MARTEN_SECRET_KEY", "")
+  config.allowed_hosts = ENV.fetch("MARTEN_ALLOWED_HOSTS", "").split(",")
   
   # Assets Settings
   config.assets.url = "/assets/"
@@ -16,7 +16,22 @@ Marten.configure :production do |config|
   config.middleware.unshift(Marten::Middleware::AssetServing)
   config.middleware.unshift(Marten::Middleware::SSLRedirect)
 
-  # # Emailing Settings
+  # Emailing Settings
   config.emailing.from_address = "noreply@kebabkhana.com"
   config.emailing.backend = Marten::Emailing::Backend::Development.new(print_emails: true)
+
+  # Database Settings
+  if ENV.has_key?("DATABASE_URL")
+    config.database do |db|
+      database_uri = URI.parse(ENV.fetch("DATABASE_URL"))
+
+      db.backend = :postgresql
+      db.host = database_uri.host
+      db.port = database_uri.port
+      db.user = database_uri.user
+      db.password = database_uri.password
+      db.name = database_uri.path[1..]
+
+      db.options = {"sslmode" => "disable"}
+    end
 end
