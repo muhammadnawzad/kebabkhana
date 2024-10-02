21375017255155e6a4240317f553bbce2967b5dd
Muhammad Nawzad <hama127n@gmail.com>
2024-10-02 09:31:56 +0300

    Updates environment settings

--- config/settings/development.cr
+++ config/settings/development.cr
@@ -7,16 +7,17 @@ Marten.configure :development do |config|
   # https://martenframework.com/docs/development/reference/settings#emailing-settings
   config.emailing.backend = Marten::Emailing::Backend::Development.new(print_emails: true)
 
+  config.assets.url = "/assets/"
   config.assets.dirs = [
     Path["src/assets/built"].expand,
-    Path["src/assets"].expand,
+    Path["src/assets/images"].expand,
   ]
 
   webpack_sock = Socket.tcp(Socket::Family::INET)
   begin
-    webpack_sock.bind("localhost", 8080)
+    webpack_sock.bind("localhost", 3000)
   rescue Socket::BindError
-    config.assets.url = "http://localhost:8080/assets/"
+    config.assets.url = "http://localhost:3000/assets/"
   end
   webpack_sock.close
 end
--- config/settings/production.cr
+++ config/settings/production.cr
@@ -1,4 +1,12 @@
 Marten.configure :production do |config|
-  config.host = "0.0.0.0"
   config.port = 3000
+  config.assets.url = "/assets/"
+  config.assets.dirs = [
+    Path["src/assets/built"].expand,
+    Path["src/assets/images"].expand,
+  ]
+  config.allowed_hosts = ENV.fetch("SELF__ALLOWED_HOSTS", "").split(",")
+  config.middleware.unshift(Marten::Middleware::AssetServing)
+  config.emailing.from_address = "noreply@kebabkhana.com"
+  config.emailing.backend = Marten::Emailing::Backend::Development.new(print_emails: true)
 end
