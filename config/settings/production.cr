d6241b380c5bb4f20c4853a39f708d6a0ba666ba
Muhammad Nawzad <hama127n@gmail.com>
2024-10-02 15:31:54 +0300

    Reverts commit

--- config/settings/production.cr
+++ config/settings/production.cr
@@ -11,7 +11,7 @@ Marten.configure :production do |config|
   
   # Middlewares Settings
   config.middleware.unshift(Marten::Middleware::AssetServing)
-  # config.middleware.unshift(Marten::Middleware::SSLRedirect)
+  config.middleware.unshift(Marten::Middleware::SSLRedirect)
 
   # Emailing Settings
   config.emailing.from_address = "noreply@kebabkhana.com"
