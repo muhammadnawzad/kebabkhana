6fbab68eba809e3741f2756d4e18cbbf9eb63197
Muhammad Nawzad <hama127n@gmail.com>
2024-10-02 09:50:13 +0300

    Adds ssl middleware

--- config/settings/production.cr
+++ config/settings/production.cr
@@ -1,12 +1,20 @@
 Marten.configure :production do |config|
+  # Application Settings
   config.port = 3000
+  config.allowed_hosts = ENV.fetch("SELF__ALLOWED_HOSTS", "").split(",")
+  
+  # Assets Settings
   config.assets.url = "/assets/"
   config.assets.dirs = [
     Path["src/assets/built"].expand,
     Path["src/assets/images"].expand,
   ]
-  config.allowed_hosts = ENV.fetch("SELF__ALLOWED_HOSTS", "").split(",")
+  
+  # Middlewares Settings
   config.middleware.unshift(Marten::Middleware::AssetServing)
+  config.middleware.unshift(Marten::Middleware::SSLRedirect)
+
+  # Emailing Settings
   config.emailing.from_address = "noreply@kebabkhana.com"
   config.emailing.backend = Marten::Emailing::Backend::Development.new(print_emails: true)
 end
