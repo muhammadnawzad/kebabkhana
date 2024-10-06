60594c10a856b2c181cea6487cda25f72efa091a
Muhammad Nawzad <hama127n@gmail.com>
2024-10-06 08:14:24 +0300

    Updates users list

--- src/apps/management/handlers/user_list_handler.cr
+++ src/apps/management/handlers/user_list_handler.cr
@@ -1,16 +1,21 @@
 module Management
   class UserListHandler < Marten::Handlers::RecordList
     property active_nav_item : String = "users"
-
+    
     include Auth::RequireSignedInUser
     include Auth::RequireAdminUser
     include NavItemActivateable
 
+    before_render :add_total_pages_to_context
+
     template_name "user/list.html"
     model Auth::User
+    page_size 10
+    ordering "first_name", "last_name"
 
-    def queryset
-      super.order("-created_at")
+    def add_total_pages_to_context
+     total_pages = ([1, queryset.count].max / @@page_size.not_nil!).ceil.to_i32
+     context[:total_pages] = total_pages
     end
   end
 end
