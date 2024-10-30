402cc8b9a124b1d57f8ef4897391e0ca8be9024f
Muhammad Nawzad <hama127n@gmail.com>
2024-10-30 10:10:23 +0300

    Allows everyone to see everyone's orders

--- src/apps/management/handlers/order_list_handler.cr
+++ src/apps/management/handlers/order_list_handler.cr
@@ -12,14 +12,6 @@ module Management
     page_size 9
     ordering "-created_at"
 
-    def queryset
-      if request.user.not_nil!.admin?
-        super
-      else
-        super.filter(user_id: request.user.not_nil!.id)
-      end
-    end
-
     private def add_total_pages_to_context
       total_pages = ([1, queryset.count].max / @@page_size.not_nil!).ceil.to_i32
       context[:total_pages] = total_pages
